5abba85fc6e883c49d6fe62e1c32ba4a0abeedbf
Advik Chitre <advik@dyn3137-243.wlan.ic.ac.uk>
2024-02-03 23:41:16 +0000

    Removed old frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DisplayScreen from "./Screens/DisplayScreen"
+import InputScreen from "./Screens/InputScreen"
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+
+          <Stack.Screen name="InputScreen" component={InputScreen} options={{headerShown: false}}/>
+          <Stack.Screen name="DisplayScreen" component={DisplayScreen} options={{headerShown: false}}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
 
+// TODO: make work with styles.js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
