5aa33d6b1558b18fec00b13a53a358e4a46b0256
Rhea Garud <rg921@ic.ac.uk>
2024-02-03 22:01:19 +0000

    Slayed error msg

--- frontend_NEW/Screens/InputScreen.js
+++ frontend_NEW/Screens/InputScreen.js
@@ -4,18 +4,27 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from '../styles';
 
+const isInputValid = (input) => {
+  return input.trim() !== '';
+};
+
 const InputScreen = () => {
   const [inputText, setInputText] = useState('');
+  const [errorText, setErrorText] = useState('');
   const navigation = useNavigation();
 
   const handleInputChange = (text) => {
     setInputText(text);
+    setErrorText('');
   };
 
   const handleButtonPress = () => {
-    console.log('READING TEXT:', '[' + inputText + ']');
-    navigation.navigate('DisplayScreen', { inputText })
-  };
+    if (isInputValid(inputText)) {
+      navigation.navigate('DisplayScreen')
+    } else {
+      setErrorText('Invalid input. Please enter a valid expression.')
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -29,6 +38,8 @@ const InputScreen = () => {
         value={inputText}
       />
 
+      <Text style={styles.errorText}>{errorText}</Text>
+
       <Button title="Parse"
         onPress={handleButtonPress}
         style = {styles.roundedButton}
@@ -37,5 +48,36 @@ const InputScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  textBox: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 8,
+    marginBottom: 16,
+    width: '100%',
+    borderRadius: 8,
+  },
+  roundedButton: {
+    backgroundColor: 'black',
+    borderRadius: 8,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 16,
+  },
+});
 
 export default InputScreen;
